c7ad4fef00f102894a6f5d4dd01c02ee6b022d64
fuad <fuadshorlarxwix@gmail.com>
2025-02-14 04:55:54 +0100

    tenzie game app done

---
+++ src/Die.jsx
@@ -0,0 +1,17 @@
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59e391" : "white",
+  };
+  return (
+    <button
+      onClick={() => props.hold(props.id)}
+      style={styles}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, ${
+        props.isHeld ? "held" : "not held"
+      }`}
+    >
+      {props.value}
+    </button>
+  );
+}
